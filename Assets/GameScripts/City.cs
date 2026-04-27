007788fe0798e1962dc5416ca138d6820ef9a9e7
clucas4 <clucas4@student.cvtc.edu>
2026-04-26 19:32:29 -0500

    Add game board functionality

---
+++ Assets/GameScripts/City.cs
@@ -0,0 +1,16 @@
+using System.Collections.Generic;
+using Unity;
+using UnityEngine;
+
+[System.Serializable]
+public class City
+{
+    public string cityName;
+    public Color diseaseColor;
+    public List<City> neighbors;
+    
+
+    public Dictionary<Color, int> infectionLevels;
+    public bool hasResearchStation = false;
+    
+}
